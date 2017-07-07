305222e57718c916b45668cdf0d71adbd34e29e0
AlejandroKantor <akantorb@gmail.com>
2017-07-07 17:38:37 -0400

    explore only p123

--- scripts/exploration.R
+++ scripts/exploration.R
@@ -1,17 +1,4 @@
-v_s_task1 <- names(dt_mturk)
 
-v_s_task2 <- names(dt_mturk_task2)
-
-intersect(v_s_task1,v_s_task2)
-
-setdiff(v_s_task1,v_s_task2)
-setdiff(v_s_task2,v_s_task1)
-
-dt_combine <- merge(dt_mturk, dt_mturk_task2, by = v_s_id, all = TRUE)
-View(dt_combine)
-
-sapply(dt_combine, function(v_value) sum(is.na(v_value)))
-sapply(dt_mturk_task2, function(v_value) sum(is.na(v_value)))
 
 dt_mturk_2010_p123_post <- l_tables[["dt_mturk_2010_p123_post"]]
 dt_mturk_2010_p123_entry <- l_tables[["dt_mturk_2010_p123_entry"]]
@@ -36,13 +23,28 @@ nrow(dt_1)
 dt_mturk_2010_p123 <- leftMerge(dt_mturk_2010_p123_entry,
                                 dt_mturk_2010_p123_average_response, 
                                 v_s_by =  c("page_id", "tab_number" ))
+
+#TODO merge dt_mturk_2010_p123_post with dt_mturk_2010_p123 twice
+# once for context and the other for response
+
+dt_mturk_2010_p123_post[ , context_triple_index := triple_index]
+dt_mturk_2010_p123_post[ , response_triple_index := triple_index]
+
+
 getSharedCols(dt_mturk_2010_p123, dt_mturk_2010_p123_post)
 
-isColsUniqueIdentifier(dt_mturk_2010_p123_post, "p123_triple_id")
-isColsUniqueIdentifier(dt_mturk_2010_p123, "p123_triple_id")
+isColsUniqueIdentifier(dt_mturk_2010_p123_post, c("p123_triple_id", "context_triple_index"))
+
+names(dt_mturk_2010_p123_post)
+
+isColsUniqueIdentifier(dt_mturk_2010_p123, "p123_triple_id",)
+
+
 
 getSharedCols(l_tables[["dt_post"]], dt_mturk_2010_p123)
 getSharedCols(l_tables[["dt_post"]], dt_mturk_2010_p123_post)
+
+
 
 dt_mturk_2010_p123[ , count_id := .N, by = "p123_triple_id"]
 i_nrow <- nrow(dt_mturk_2010_p123)
