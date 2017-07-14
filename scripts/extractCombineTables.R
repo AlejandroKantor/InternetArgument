ab4ff3608a34ee1e2080885aa2dde11a31227275
AlejandroKantor <akantorb@gmail.com>
2017-07-14 15:10:08 -0400

    Add task3 ie p123 data set creation

--- scripts/extractCombineTables.R
+++ scripts/extractCombineTables.R
@@ -70,6 +70,11 @@ names(dt_quote)
 dt_mturk <- l_tables[["dt_mturk_2010_qr_entry"]]
 dt_mturk_task1 <- l_tables[["dt_mturk_2010_qr_task1_average_response"]]
 dt_mturk_task2 <- l_tables[["dt_mturk_2010_qr_task2_average_response"]]
+
+dt_mturk_2010_p123_post <- l_tables[["dt_mturk_2010_p123_post"]]
+dt_mturk_2010_p123_entry <- l_tables[["dt_mturk_2010_p123_entry"]]
+dt_mturk_2010_p123_average_response <- l_tables[["dt_mturk_2010_p123_average_response"]]
+
 setnames(dt_mturk_task1, "num_annots", "num_annots_task1")
 setnames(dt_mturk_task2, "num_annots", "num_annots_task2")
 
@@ -92,8 +97,30 @@ isColsUniqueIdentifier(dt_quote, v_s_id)
 #dt_quote <- leftMerge(dt_quote, dt_mturk, v_s_id)
 dt_mturk <- leftMerge( dt_mturk, dt_quote, v_s_id)
 
+# ------- p123
 
+dt_mturk_2010_p123 <- leftMerge(dt_mturk_2010_p123_entry,
+                                dt_mturk_2010_p123_average_response, 
+                                v_s_by =  c("page_id", "tab_number" ))
 
+dt_mturk_p123_meta <- unique(dt_mturk_2010_p123_post[ , .(p123_triple_id, discussion_id, topic  )])
+
+v_s_cases <- c("context", "response")
+
+for(s_case in v_s_cases){
+  dt_mturk_2010_p123_post_copy <- copy(dt_mturk_2010_p123_post)
+  v_s_id <- c("p123_triple_id", paste0(s_case,"_","triple_index"))
+  v_s_other_cols <- setdiff(names(dt_mturk_2010_p123_post_copy), "p123_triple_id")
+  setnames(dt_mturk_2010_p123_post_copy, v_s_other_cols, paste0(s_case,"_",v_s_other_cols))
+  dt_mturk_2010_p123 <- leftMerge(dt_mturk_2010_p123, dt_mturk_2010_p123_post_copy, v_s_id)
+}
+
+# remove some redundant columns
+dt_mturk_2010_p123[ , topic_mturk:= context_topic ]
+dt_mturk_2010_p123[ , c("context_topic", "response_topic") := NULL ]
+
+dt_mturk_2010_p123[ , discussion_id := context_discussion_id]
+dt_mturk_2010_p123[ , c("context_discussion_id", "response_discussion_id") := NULL ]
 
 #------------------------------------------------------------------------------------------------------
 #topic 
@@ -101,6 +128,8 @@ dt_topic <- leftMerge( l_tables[["dt_discussion_topic"]], l_tables[["dt_topic"]]
 
 setnames(dt_mturk, "topic", "topic_mturk")
 dt_mturk <- leftMerge(dt_mturk, dt_topic, "discussion_id")
+
+dt_mturk_2010_p123 <- leftMerge(dt_mturk_2010_p123, dt_topic, "discussion_id")
 
 #------------------------------------------------------------------------------------------------------
 
@@ -164,14 +193,45 @@ v_s_order <- c("post_id",
               
 )
 
-setcolorder(dt_mturk, v_s_order)
+v_s_order_p123 <- c( "p123_triple_id", 
+                    "context_triple_index", 
+                    "response_triple_index", 
+                    "context_post_id", 
+                    "response_post_id", 
+                    "discussion_id", 
+                    "page_id", 
+                    "context_presented_text", 
+                    "context_presented_text_term_removed", 
+                    "context_term", 
+                    "response_presented_text", 
+                    "response_presented_text_term_removed", 
+                    "response_term", 
+                    "topic_mturk",
+                    "topic_id", 
+                    "topic", 
+                    "tab_number", 
+                    "num_annots", 
+                    "disagree_agree", 
+                    "disagree_agree_unsure", 
+                    "attacking_respectful", 
+                    "attacking_respectful_unsure", 
+                    "emotion_fact", 
+                    "emotion_fact_unsure", 
+                    "nasty_nice", 
+                    "nasty_nice_unsure", 
+                    "sarcasm_yes", 
+                    "sarcasm_no", 
+                    "sarcasm_unsure"
+                    )
 
-# set.seed(1111)
-# i_rows_example = 2000
-# v_i_sample <- sample(x = 1:nrow(dt_quote),size = i_rows_example)
+setcolorder(dt_mturk, v_s_order)
+setcolorder(dt_mturk_2010_p123, v_s_order_p123)
 
 write.csv(dt_mturk, file="./data/output/mturkTask1And2.csv" ,row.names = FALSE)
 save(dt_mturk, file="./data/output/mturkTask1And2.RData" )
+
+write.csv(dt_mturk_2010_p123, file="./data/output/mturkTask3.csv" ,row.names = FALSE)
+save(dt_mturk_2010_p123, file="./data/output/mturkTask3.RData" )
 
 # l_tables <- list()
 # dt_agg <- dt_quote[ ,.(count = .N, 
